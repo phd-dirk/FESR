ba3d83c5af99ba13ba2e31d0a6348ec2c40f46b1
knowledge91 <dirkhornung91@gmail.com>
2018-03-02 14:45:06 +0100

    deleted CRunDec & included MINUIT example

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
-#include "./src/data.hpp"
-#include "./src/s0_sets.hpp"
-#include "./src/state.hpp"
-#include "./src/weights.hpp"
-#include "./src/numerics.hpp"
-#include <boost/numeric/ublas/matrix.hpp>
-#include <gsl/gsl_integration.h>
 #include <iostream>
-#include <cstdio>
-#include <fstream>
-#include <string>
-#include <complex>
-#include <cmath>
-
-#include "./src/theoretical_moments.hpp"
 
 using std::cout;
 using std::endl;
 
+// MINUIT
+#include "Math/Minimizer.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+#include "TRandom2.h"
+#include "TError.h"
+
+using ROOT::Math::Minimizer;
+using ROOT::Math::Factory;
+using ROOT::Math::Functor;
+
+double RosenBrock(const double *xx) {
+  const Double_t x = xx[0];
+  const Double_t y = xx[1];
+  const Double_t tmp1 = y-x*x;
+  const Double_t tmp2 = 1-x;
+  return 100*tmp1*tmp1*tmp2*tmp2;
+}
+
 int main () {
   cout.precision(17);
+
+  Minimizer* min = Factory::CreateMinimizer("Minuit2", "Migrad");
+
+  // set tolerances
+  min->SetMaxFunctionCalls(10000000); // for Minuit2
+  min->SetMaxIterations(10000000); // for GSL
+  min->SetTolerance(1e-17);
+  // min->SetPrintLevel(1); // activate logging
+
+  // function wrapper
+  Functor f(&RosenBrock, 2);
+  double step[2] = {0.01, 0.01};
+  // starting point
+  double variable[2] = {-1., 1.2};
+
+  min->SetFunction(f);
+
+  // set free variables to be minimized
+  min->SetVariable(0, "x", variable[0], step[0]);
+  min->SetVariable(1, "y", variable[1], step[1]);
+
+  // minimize!
+  min->Minimize();
+
+  const double *xs = min->X();
+  cout << "Minimum f(" << xs[0] << "," << xs[1] << "): "
+       << min->MinValue() << endl;
+
+  cout << "min " << RosenBrock(xs) << endl;
 
   return 0;
 }
