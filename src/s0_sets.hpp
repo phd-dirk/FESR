38d0e431d4866aa32050329331da6cf4ec86c1d9
knowledge91 <dirkhornung91@gmail.com>
2018-06-13 14:09:40 +0200

    removed s0_sets.hpp and contants.hpp

--- src/s0_sets.hpp
+++
@@ -1,9 +0,0 @@
-#import <vector>
-#import <cmath>
-#import "./constants.hpp"
-
-using std::vector;
-using std::pow;
-
-const vector<double> s0Set { 3.1570893124, 3., 2.8, 2.6, 2.4, 2.3, 2.2, 2.1, 2. };
-const vector<double> s0Set8 { 3.1570893124, 3., 2.8, 2.6, 2.4, 2.3, 2.2, 2.1 };
