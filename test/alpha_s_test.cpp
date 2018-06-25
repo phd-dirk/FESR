f2fbe112925d10764c06e4cd83bd0f5fd6b6197e
knowledge91 <dirkhornung91@gmail.com>
2018-06-25 20:13:22 +0200

    added complex adler running test

--- test/alpha_s_test.cpp
+++ test/alpha_s_test.cpp
@@ -20,4 +20,7 @@ TEST_F(AlphaSTest, complex) {
   q = cmplx(3.0, 1.5);
   EXPECT_NEAR((*amu_)(q, 3.1, 0.1028).real(), 0.09858208182028837, 1e-14);
   EXPECT_NEAR((*amu_)(q, 3.1, 0.1028).imag(), -0.01286649420615981, 1e-14);
-}
+
+  EXPECT_NEAR((*amu_)(cmplx(3.0, 2.0), 3.1, 0.1028).real(), 0.09567038211680548, 1e-14);
+  EXPECT_NEAR((*amu_)(cmplx(3.0, 2.0), 3.1, 0.1028).imag(), -0.015397224860415053, 1e-14);
+};
