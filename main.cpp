a7cbe9bcc4152da5c3fba82f8c9cbe658d0ff6c8
knowledge91 <dirkhornung91@gmail.com>
2018-09-14 16:33:06 +0200

    write vars to output file (astau, aGG, rho, c8 with errors)

--- main.cpp
+++ main.cpp
@@ -97,7 +97,7 @@ int dof(const Configuration config) {
   return dof;
 }
 
-void writeOutput(const string configFilePath, const string outputFilePath) {
+void writeOutput(const string configFilePath, const string outputFilePath, Minimizer* min) {
   ifstream configFile;
   configFile.open(configFilePath);
   ofstream outputFile;
@@ -107,15 +107,27 @@ void writeOutput(const string configFilePath, const string outputFilePath) {
   string line;
   if (configFile.is_open())
   {
-    while( getline (configFile, line) )
+    if(outputFile.is_open())
     {
-      outputFile << line << "\n";
+      // write config.json to output file
+      while( getline (configFile, line) )
+      {
+        outputFile << line << "\n";
+      }
+
+      const double *xs = min->X();
+      const double *errors = min->Errors();
+
+      outputFile << "astau: \t = " << xs[0] << "\t +/- \t " << errors[0] << "\n";
+      outputFile << "aGG: \t = " << xs[1] << "\t +/- \t " << errors[1] << "\n";
+      outputFile << "rho: \t = " << xs[2] << "\t +/- \t " << errors[2] << "\n";
+      outputFile << "c8: \t = " << xs[3] << "\t +/- \t " << errors[3] << "\n";
+
+      outputFile << endl;
+      outputFile.close();
     }
     configFile.close();
   }
-
-  outputFile << endl;
-  outputFile.close();
 }
 
 int main (int argc, char* argv[]) {
@@ -139,54 +151,54 @@ int main (int argc, char* argv[]) {
   // cout << dv.moment(1., Weight(1), 3.0, 0.0018963380072094527, -2.2, 3.9) << endl;
 
   // MINUIT
-  // Minimizer* min = Factory::CreateMinimizer("Minuit2", "Migrad");
+  Minimizer* min = Factory::CreateMinimizer("Minuit2", "Migrad");
 
-  // // set tolerances
-  // // min->SetMaxFunctionCalls(10000000); // for Minuit2
-  // // min->SetMaxIterations(10000000); // for GSL
-  // min->SetTolerance(1e-15);
-  // min->SetStrategy(2);
-  // min->SetPrintLevel(3); // activate logging
+  // set tolerances
+  // min->SetMaxFunctionCalls(10000000); // for Minuit2
+  // min->SetMaxIterations(10000000); // for GSL
+  min->SetTolerance(1e-15);
+  min->SetStrategy(2);
+  min->SetPrintLevel(3); // activate logging
 
-  // // function wrapper
-  // Functor chi2(chisquared, 4);
+  // function wrapper
+  Functor chi2(chisquared, 4);
 
-  // min->SetFunction(chi2);
+  min->SetFunction(chi2);
 
-  // // set free variables to be minimized
-  // if (config.astau.isFixed) {
-  //   min->SetFixedVariable(0, "astau", config.astau.value);
-  // } else {
-  //   min->SetVariable(0, "astau", config.astau.value, config.astau.stepSize);
-  // }
-  // if (config.aGGInv.isFixed) {
-  //   min->SetFixedVariable(1, "aGGInv", config.aGGInv.value);
-  // } else {
-  //   min->SetVariable(1, "aGGInv", config.aGGInv.value, config.aGGInv.stepSize);
-  // }
-  // if (config.rhoVpA.isFixed) {
-  //   min->SetFixedVariable(2, "rhoVpA", config.rhoVpA.value);
-  // } else {
-  //   min->SetVariable(2, "rhoVpA", config.rhoVpA.value, config.rhoVpA.stepSize);
-  // }
-  // if (config.c8VpA.isFixed) {
-  //   min->SetFixedVariable(3, "c8VpA", config.c8VpA.value);
-  // } else {
-  //   min->SetVariable(3, "c8VpA", config.c8VpA.value, config.c8VpA.stepSize);
-  // }
-  // // min->SetVariable(4, "vKappa", 3.0, 0.1);
-  // // min->SetVariableLowerLimit(4, 0);
-  // // min->SetVariable(5, "vGamma", 1.2, 0.1);
-  // // min->SetFixedVariable(6, "vAlpha", -2.2);
-  // // min->SetFixedVariable(7, "vBeta", 3.9);
-  // // min->SetVariable(8, "aKappa", 3.0, 0.1);
-  // // min->SetVariableLowerLimit(8, 0);
-  // // min->SetFixedVariable(9, "aGamma", 1.3);
-  // // min->SetFixedVariable(10, "aAlpha", 4.7);
-  // // min->SetFixedVariable(11, "aBeta", 1.8);
+  // set free variables to be minimized
+  if (config.astau.isFixed) {
+    min->SetFixedVariable(0, "astau", config.astau.value);
+  } else {
+    min->SetVariable(0, "astau", config.astau.value, config.astau.stepSize);
+  }
+  if (config.aGGInv.isFixed) {
+    min->SetFixedVariable(1, "aGGInv", config.aGGInv.value);
+  } else {
+    min->SetVariable(1, "aGGInv", config.aGGInv.value, config.aGGInv.stepSize);
+  }
+  if (config.rhoVpA.isFixed) {
+    min->SetFixedVariable(2, "rhoVpA", config.rhoVpA.value);
+  } else {
+    min->SetVariable(2, "rhoVpA", config.rhoVpA.value, config.rhoVpA.stepSize);
+  }
+  if (config.c8VpA.isFixed) {
+    min->SetFixedVariable(3, "c8VpA", config.c8VpA.value);
+  } else {
+    min->SetVariable(3, "c8VpA", config.c8VpA.value, config.c8VpA.stepSize);
+  }
+  // min->SetVariable(4, "vKappa", 3.0, 0.1);
+  // min->SetVariableLowerLimit(4, 0);
+  // min->SetVariable(5, "vGamma", 1.2, 0.1);
+  // min->SetFixedVariable(6, "vAlpha", -2.2);
+  // min->SetFixedVariable(7, "vBeta", 3.9);
+  // min->SetVariable(8, "aKappa", 3.0, 0.1);
+  // min->SetVariableLowerLimit(8, 0);
+  // min->SetFixedVariable(9, "aGamma", 1.3);
+  // min->SetFixedVariable(10, "aAlpha", 4.7);
+  // min->SetFixedVariable(11, "aBeta", 1.8);
 
-  // // minimize!
-  // min->Minimize();
+  // minimize!
+  min->Minimize();
   // std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
   // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()*1e-6  <<std::endl;
   // const double *xs = min->X();
@@ -196,6 +208,6 @@ int main (int argc, char* argv[]) {
 
 
 
-  writeOutput("./configuration.json", outputFilePath);
+  writeOutput("./configuration.json", outputFilePath, min);
   return 0;
 }
