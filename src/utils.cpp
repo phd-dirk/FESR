b304751a4c72468401652244b7ab342e930ace8c
knowledge91 <dirkhornung91@gmail.com>
2018-09-26 15:13:56 +0200

    refactoring + (fixed) variable output

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,6 +8,29 @@ void addCSV(double value, ofstream &outputFile, bool last = false) {
   }
 }
 
+string variableOutput(Minimizer* min, const uint &i) {
+  const bool isFixed = min->IsFixedVariable(i);
+  const double *xs = min->X();
+  const double *errors = min->Errors();
+  string out = "";
+
+  // name
+  out += min->VariableName(i) + "\t=\t" + std::to_string(xs[0]);
+
+  // error
+  if(!isFixed) {
+    out += "\t+/-\t" + std::to_string(errors[0]) + "\t";
+  }
+
+  // fixed
+  if(isFixed) {
+    out += "\t(fixed)";
+  }
+
+  out += "\n";
+  return out;
+}
+
 void writeToCSV(
     string &csvFilePath,
     Minimizer* min,
@@ -49,14 +72,21 @@ void writeToCSV(
 }
 
 void writeToOutputFile(
-    string &outputFilePath,
+    const string &configFilePath,
     Minimizer* min
 ) {
+  std::size_t pos = configFilePath.find_last_of("/\\");
+  pos = configFilePath.find_last_of(".");
+  std::string outputFilePath = configFilePath.substr(0,pos) + ".txt";
+
   ofstream outputFile;
   outputFile.open(outputFilePath);
   outputFile << std::setprecision(15);
 
-  if(outputFile.is_open())
+  ifstream configFile;
+  configFile.open(configFilePath);
+
+  if(outputFile.is_open() && configFile.is_open())
   {
     outputFile << "Minuit2Minimizer\t Status:\t" << min->Status() << endl;
     outputFile << "FVAL\t=\t" << min->MinValue() << endl;
@@ -65,21 +95,18 @@ void writeToOutputFile(
 
     const double *xs = min->X();
     const double *errors = min->Errors();
-    outputFile << "astau\t=\t" << xs[0] << "\t+/-\t" << errors[0] << endl;
-    outputFile << "aGGInv\t=\t" << xs[1] << "\t+/-\t" << errors[1] << endl;
-    outputFile << "O6\t=\t" << xs[2] << "\t+/-\t" << errors[2] << endl;
-    outputFile << "O8\t=\t" << xs[3] << "\t+/-\t" << errors[3] << endl;
 
-    // DV
-    outputFile << "deltaV\t=\t" << xs[4] << "\t+/-\t" << errors[4] << endl;
-    outputFile << "gammaV\t=\t" << xs[5] << "\t+/-\t" << errors[5] << endl;
-    outputFile << "alphaV\t=\t" << xs[6] << "\t+/-\t" << errors[6] << endl;
-    outputFile << "betaV\t=\t" << xs[7] << "\t+/-\t" << errors[7] << endl;
-    outputFile << "deltaA\t=\t" << xs[8] << "\t+/-\t" << errors[8] << endl;
-    outputFile << "gammaA\t=\t" << xs[9] << "\t+/-\t" << errors[9] << endl;
-    outputFile << "alphaA\t=\t" << xs[10] << "\t+/-\t" << errors[10] << endl;
-    outputFile << "betaA\t=\t" << xs[11] << "\t+/-\t" << errors[11] << endl;
+    // output variables
+    for(uint i = 0; i <= 11; i++) {
+      outputFile << variableOutput(min, i);
+    }
 
+    outputFile << endl;
+
+    // add configFile
+    outputFile << configFile.rdbuf();
+
+    configFile.close();
     outputFile.close();
   }
 }
@@ -91,11 +118,9 @@ void writeOutput (
 ) {
   std::size_t pos = configFilePath.find_last_of("/\\");
   std::string csvFilePath = configFilePath.substr(0,pos) + "/fits.csv";
-  pos = configFilePath.find_last_of(".");
-  std::string outputFilePath = configFilePath.substr(0,pos) + ".txt";
 
   writeToCSV(csvFilePath, min, config);
-  writeToOutputFile(outputFilePath, min);
+  writeToOutputFile(configFilePath, min);
 }
 
 
