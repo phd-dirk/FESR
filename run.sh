c95b767a96c4e3fb823b90eb6c1960889403af01
Dirk Hornung <dirkhornung91@gmail.com>
2018-11-12 15:24:11 +0100

    run.sh implement c10 and c12 in csv header

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DATE=`date '+%Y-%m-%d_%H:%M:%S'`
 OUTPUT_FILE="$1/fits.csv"
 
 echo $OUTPUT_FILE
-echo -e "$DATE \n"'status,alpha,alphaErr,aGGInv,aGGInvErr,c6,c6Err,c8,c8Err,chi,dof,chiDof,edm' > $OUTPUT_FILE
+echo -e "$DATE \n"'status,alpha,alphaErr,aGGInv,aGGInvErr,c6,c6Err,c8,c8Err,c10,c10Err,chi,dof,chiDof,edm' > $OUTPUT_FILE
 for i in $( ls $1 ); do
     if [[ $i == *.json ]]; then
         echo $i
